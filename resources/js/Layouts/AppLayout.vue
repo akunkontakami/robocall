86e901fe092158348d7e0e3075aa7cc046175c85
Naufal Hanif <nahanifg@gmail.com>
2026-06-19 13:34:03 +0700

    update: hide dialer and robocall menu

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -31,28 +31,28 @@ defineProps(["title", "header", "headerBackUrl"]);
 const menus = ref([
     {
         name: "PDS",
-        route: route('pds.dashboard'),
+        route: route("pds.dashboard"),
         active: "pds.*",
         icon: shallowRef(IconMenuPds),
         enable: true,
         show: true,
     },
-    {
-        name: "Setup Status Auto Dial",
-        route: route('auto-dial.index'),
-        active: "auto-dial.*",
-        icon: shallowRef(IconMenuAutoDial),
-        enable: true,
-        show: true,
-    },
-    {
-        name: "Robocall",
-        route: route('robocall.dashboard'),
-        active: "robocall.*",
-        icon: shallowRef(IconMenuRoboCall),
-        enable: true,
-        show: true,
-    },
+    // {
+    //     name: "Setup Status Auto Dial",
+    //     route: route('auto-dial.index'),
+    //     active: "auto-dial.*",
+    //     icon: shallowRef(IconMenuAutoDial),
+    //     enable: true,
+    //     show: true,
+    // },
+    // {
+    //     name: "Robocall",
+    //     route: route('robocall.dashboard'),
+    //     active: "robocall.*",
+    //     icon: shallowRef(IconMenuRoboCall),
+    //     enable: true,
+    //     show: true,
+    // },
 ]);
 
 const page = usePage();
@@ -70,7 +70,7 @@ watch(
     () => page.props.flash,
     (newValue, oldValue) => {
         watchAlert();
-    }
+    },
 );
 
 onMounted(() => {
