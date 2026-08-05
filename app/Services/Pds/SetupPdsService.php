c464f3ff37b0f0df70061635065597c80587aa2f
hilman5788 <hilman5788@gmail.com>
2026-08-05 13:47:01 +0700

    update

--- app/Services/Pds/SetupPdsService.php
+++ app/Services/Pds/SetupPdsService.php
@@ -48,7 +48,7 @@ class SetupPdsService
             ->when($pds, fn($q) => $q->whereIn('id', $pds))
             ->when($search, fn($q) => $q->where('pds_name', 'LIKE', "%$search%"))
             ->orderBy('created_at', 'desc');
-
+            dump($data->toSql(), $data->getBindings());
         if ($limit === null) {
             $data = $data->get();
         } else {
@@ -101,7 +101,7 @@ class SetupPdsService
             )
             ->when($search, fn($q) => $q->whereHas('pds', fn($q2) => $q2->where('pds_name', 'LIKE', "%$search%")))
             ->orderBy('created_at', 'desc');
-        dump($data->toSql(), $data->getBindings());
+        // dump($data->toSql(), $data->getBindings());
         
         if ($limit === null) {
             $data = $data->get();
