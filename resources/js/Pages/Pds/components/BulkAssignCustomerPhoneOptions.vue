f6e3d72fb40f88f5dd3428611ec56ae3da3c37ef
lalilmahfud <laililmahvut@gmail.com>
2026-08-13 15:01:14 +0700

    feat : styling

--- resources/js/Pages/Pds/components/BulkAssignCustomerPhoneOptions.vue
+++ resources/js/Pages/Pds/components/BulkAssignCustomerPhoneOptions.vue
@@ -1,8 +1,11 @@
 <template>
     <div>
-        <label class="flex cursor-pointer items-center gap-3">
+        <div class="flex items-center">
+
+        <label class="flex gap-x-3 cursor-pointer group">
             <input
                 type="checkbox"
+                class="!m-0 shrink-0"
                 :checked="modelValue.length > 0"
                 @change="toggleAll"
             />
@@ -10,22 +13,23 @@
                 {{ label }}
             </span>
         </label>
+        </div>
 
         <div
             v-if="modelValue.length > 0"
-            class="ml-4 mt-3 grid grid-cols-2 gap-3 rounded-lg border bg-slate-50 p-4 sm:grid-cols-3"
+            class="ml-4 grid grid-cols-2 sm:grid-cols-3 gap-3 p-4 bg-slate-50 rounded-lg border mt-3"
         >
             <label
                 v-for="number in options"
                 :key="number"
-                class="flex cursor-pointer items-center gap-2 text-xs text-slate-600"
+                class="flex gap-x-2 cursor-pointer group"
             >
                 <input
                     type="checkbox"
                     :checked="modelValue.includes(`${prefix} ${number}`)"
                     @change="toggle(`${prefix} ${number}`)"
                 />
-                <span>{{ prefix }} {{ number }}</span>
+                <span class="text-xs font-opensauceone-medium text-slate-500 group-hover:text-slate-700 transition-colors">{{ prefix }} {{ number }}</span>
             </label>
         </div>
     </div>
