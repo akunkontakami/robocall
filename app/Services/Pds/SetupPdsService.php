a98aba5304868407460f46109d4d25dc437407c9
hilman5788 <hilman5788@gmail.com>
2026-08-20 14:47:44 +0700

    update

--- app/Services/Pds/SetupPdsService.php
+++ app/Services/Pds/SetupPdsService.php
@@ -1027,7 +1027,7 @@ class SetupPdsService
                 DB::raw("COUNT(DISTINCT th.ticket_id) as data_contacted"),
             ])
             ->groupBy(DB::raw('DATE(th.created_at)'), 'ca.agent_id');
-            // dump($aggQuery->toSql(), $aggQuery->getBindings());
+            dump($aggQuery->toSql(), $aggQuery->getBindings());
         $aggRowsList = $aggQuery->get();
         $aggByDateUser = [];
         $aggUserIds = [];
