8e2390617e3b95e9c0c127df03243f21b8107154
hilman5788 <hilman5788@gmail.com>
2026-08-21 11:21:07 +0700

    update

--- app/Services/Pds/SetupPdsService.php
+++ app/Services/Pds/SetupPdsService.php
@@ -1085,7 +1085,7 @@ class SetupPdsService
                     ->whereIn('th.ticket_id', $matchedTicketIds)
                     ->select('th.status', 'th.ticket_id', 'company_users.name as agent_username','company_users.code as agent_name','th.agent_id as agent_id','company_users.name as DeskCollectorName')
                     ->orderByDesc('th.created_at');
-                    // dump($ticketStatus->toSql(), $ticketStatus->getBindings());
+
                     $ticketStatus->get()
                     ->unique('ticket_id')
                     ->values();
